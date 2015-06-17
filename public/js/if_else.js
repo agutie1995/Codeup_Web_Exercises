5887767f5a1b462a10b617907904132b303d7f0b
Alexandra Gutierrez <agutie95@yahoo.com>
2015-06-17 12:06:43 -0500

    added content

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -6,39 +6,29 @@ var color = colors[Math.floor(Math.random()*colors.length)];
 var favorite = 'blue'; // TODO: change this to your favorite color from the list
 
 // TODO: Create a block of if/else statements to check for every color except indigo and violet.
-if (favorite) {
-	console.log(Blue is my favorite color.)
-} else (color == 'red', 'orange', 'yellow', 'green') {
-	console.log(This is not my favorite color.)
-}
+
 
 
 // TODO: When a color is encountered log a message that tells the color, and an object of that color.
 //       Example: Blue is the color of the sky.
-if (favorite) {
-	message = "Blue is the color of the sky."
-} else (color = 'red') {
-	message = "Red is the color of roses."
-} else (color = 'orange') {
-	message = "Orange is the color of a basketball."
-} else (color = 'yellow') {
-	message = "Yellow is the color of the sun."
-} else (color = 'green') {
-	message = "Green is the color of grass."
+if (color = 'blue') {
+	console.log("Blue is the color of the sky.")
+} else if (color = 'red') {
+	console.log("Red is the color of roses.")
+} else if (color = 'orange') {
+	console.log("Orange is the color of a basketball.")
+} else if (color = 'yellow') {
+	console.log("Yellow is the color of the sun.")
+} else if (color = 'green') {
+	console.log("Green is the color of grass.")
+} else {
+	console.log("I do not know anything by that color.")
 }
 
 // TODO: Have a final else that will catch indigo and violet.
 // TODO: In the else, log: I do not know anything by that color.
-if (favorite) {
-	console.log(Blue is my favorite color.)
-} else (color == 'red', 'orange', 'yellow', 'green', 'indigo', 'violet') {
-	console.log(I do not know anything by that color.)
-}
+
 
 // TODO: Using the ternary operator, conditionally log a statement that
 //       says whether the random color matches your favorite color.
-if (favorite) {
-	message = "Yes, this is it!"
-} else {
-	message = "Nope. Not it."
-}
+var message = (favorite) ? "Yes, this is it!" : "Nope. Not it."
