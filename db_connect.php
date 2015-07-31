10545abdf2ed91c2e4751ee94ca81d0ebc56eaf4
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-31 15:15:08 -0500

    used constants for db host, name, user and password

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,16 @@
 <?php
+// The IP address to connect to
+define('DB_HOST', '127.0.0.1');
+
+// The database to connect to
+define('DB_NAME', 'employees');
+
+// The MySQL user to use
+define('DB_USER', 'codeup');
+
+// Password for the MySQL user
+define('DB_PASS', 'password');
+
 $dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
