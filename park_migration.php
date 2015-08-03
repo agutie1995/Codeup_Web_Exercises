559a2f98fd9b0d5c35ede933dd72e764f5b27806
Alexandra Gutierrez <agutie95@yahoo.com>
2015-08-03 10:34:33 -0500

    added foreach on park_seeder.php

--- park_migration.php
+++ park_migration.php
@@ -1,21 +1,11 @@
 <?php
-// The IP address to connect to
-define('DB_HOST', '127.0.0.1');
-
-// The database to connect to
-define('DB_NAME', 'parks_db');
-
-// The MySQL user to use
-define('DB_USER', 'codeup');
-
-// Password for the MySQL user
-define('DB_PASS', 'password');
-
+require_once 'parks_config.php';
 require_once 'db_connect.php';
 
-$dbc->exec('DROP TABLE IF EXISTS national_parks');
+$dropQuery = ('DROP TABLE IF EXISTS national_parks');
+$dbc->exec($dropQuery);
 
-$query = 'CREATE TABLE national_parks(
+$createQuery = 'CREATE TABLE national_parks(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	name varchar (50) NOT NULL,
 	location varchar(50) NOT NULL,
@@ -24,4 +14,4 @@ $query = 'CREATE TABLE national_parks(
 	PRIMARY KEY (id)
 )';
 
-$dbc->exec($query);
+$dbc->exec($createQuery);
--- park_seeder.php
+++ park_seeder.php
@@ -1,19 +1,28 @@
 <?php
-require_once 'park_migration.php';
+require_once 'parks_config.php';
+require_once 'db_connect.php';
 
-$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-
-$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$query = 'TRUNCATE national_parks';
+$dbc->exec($query);
 
 $parks = [
-	['name' => 'Acadia', 'location' => 'Maine', 'date_established' => '1919-02-26', 'area_in_acres' => '47,389.67'],
+	['name' => 'Acadia', 'location' => 'Maine', 'date_established' => '1919-02-26', 'area_in_acres' => '47389.67'],
 	['name' => 'American Samoa', 'location' => 'American Samoa', 'date_established' => '1988-10-31', 'area_in_acres' => '9000.00'],
-	['name' => 'Arches', 'location' => 'Utah', 'date_established' => '1929-04-12', 'area_in_acres' => '76,518.98'],
-	['name' => 'Badlands', 'location' => 'South Dakota', 'date_established' => '1978-11-10', 'area_in_acres' => '242,755.94'],
-	['name' => 'Big Bend', 'location' => 'Texas', 'date_established' => '1944-06-12', 'area_in_acres' => '801,163.21'],
-	['name' => 'Biscayne', 'location' => 'Florida', 'date_established' => '1980-06-28', 'area_in_acres' => '172,924.07'],
-	['name' => 'Black Canyon of the Gunnison', 'location' => 'Colorado', 'date_established' => '1999-10-21', 'area_in_acres' => '32,950.03'],
-	['name' => 'Bryce Canyon', 'location' => 'Utah', 'date_established' => '1928-02-25', 'area_in_acres' => '35,835.08'],
-	['name' => 'Carlsbad Caverns', 'location' => 'New Mexico', 'date_established' => '1980-03-05', 'area_in_acres' => '46,766.45'],
-	['name' => 'Channel Islands', 'location' => 'California', 'date_established' => '1980-03-05', 'area_in_acres' => '249,561.00'],
+	['name' => 'Arches', 'location' => 'Utah', 'date_established' => '1929-04-12', 'area_in_acres' => '76518.98'],
+	['name' => 'Badlands', 'location' => 'South Dakota', 'date_established' => '1978-11-10', 'area_in_acres' => '242755.94'],
+	['name' => 'Big Bend', 'location' => 'Texas', 'date_established' => '1944-06-12', 'area_in_acres' => '801163.21'],
+	['name' => 'Biscayne', 'location' => 'Florida', 'date_established' => '1980-06-28', 'area_in_acres' => '172924.07'],
+	['name' => 'Black Canyon of the Gunnison', 'location' => 'Colorado', 'date_established' => '1999-10-21', 'area_in_acres' => '32950.03'],
+	['name' => 'Bryce Canyon', 'location' => 'Utah', 'date_established' => '1928-02-25', 'area_in_acres' => '35835.08'],
+	['name' => 'Carlsbad Caverns', 'location' => 'New Mexico', 'date_established' => '1980-03-05', 'area_in_acres' => '46766.45'],
+	['name' => 'Channel Islands', 'location' => 'California', 'date_established' => '1980-03-05', 'area_in_acres' => '249561.00'],
 ];
+
+foreach ($parks as $park) {
+    $query = "INSERT INTO national_parks (name, location, date_established, area_in_acres)
+    	VALUES ('{$park['name']}', '{$park['location']}', '{$park['date_established']}', '{$park['area_in_acres']}')";
+
+    $dbc->exec($query);
+
+    echo "Inserted ID: " . $dbc->lastInsertId() . PHP_EOL;
+}
