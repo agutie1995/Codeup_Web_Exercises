38ee8621b61c1c9c08af1fc52c64986c32b3dd2a
Alexandra Gutierrez <agutie95@yahoo.com>
2015-08-03 13:49:12 -0500

    modified pages to display four parks at a time, added links to navigate through pages

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,9 +1,14 @@
 <?php
-
 require_once '../parks_config.php';
 require_once '../db_connect.php';
 
-$parks = $dbc->query('SELECT * FROM national_parks')->fetchAll(PDO::FETCH_ASSOC);
+$offset= isset($_GET['offset']) ? intval($_GET['offset']) : 0;
+
+$stmt = $dbc->query("SELECT * FROM national_parks LIMIT 4 OFFSET $offset");
+
+$parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$count= $dbc->query('SELECT count(*) FROM national_parks')->fetchColumn();
 ?>
 
 <!DOCTYPE html>
@@ -16,18 +21,27 @@ $parks = $dbc->query('SELECT * FROM national_parks')->fetchAll(PDO::FETCH_ASSOC)
         <? foreach ($parks as $key => $park): ?>
             <tr>
                 <th><?= $park['name']; ?><th>
-            </tr>
-            <tr>
-                <td><?= $park['location']; ?></th>
-            </tr>
-            <tr>
-                <td><?= $park['date_established']; ?></th>
-            </tr>
-            <tr>
-                <td><?= $park['area_in_acres']; ?></th>
+                <td><?= $park['location']; ?></td>
+                <td><?= $park['date_established']; ?></td>
+                <td><?= $park['area_in_acres']; ?></td>
             </tr>
         <? endforeach; ?>
-
     </table>
+
+    <? if ($offset != 0):?>
+        <a href="?offset=<?=$offset-4;?>">Prev</a>
+    <? endif; ?> 
+
+    <a href="?offset=0">1</a>
+
+    <a href="?offset=4">2</a>
+    
+    <a href="?offset=8">3</a>
+
+    <? if (($offset + 4)< $count): ?>
+        <a href="?offset=<?=$offset+4;?>">Next</a> 
+    <? endif; ?>
+
+</script>
 </body>
 </html>
