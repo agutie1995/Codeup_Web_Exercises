3b2e362d460c485657eb4190bfbcf2150117de0f
Alexandra Gutierrez <agutie95@yahoo.com>
2015-08-04 16:09:56 -0500

    centerd form

--- public/national_parks.php
+++ public/national_parks.php
@@ -5,8 +5,12 @@ require_once '../function.php';
 require_once '../Input.php';
 
 $limit = 4;
-$page = isset($_GET['page']) ? intval($_GET['page']) : 0;
-$offset = $page * 4;
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+$offset = ($page - 1) * $limit;
+
+$count = $dbc->query('SELECT count(*) FROM national_parks')->fetchColumn();
+
+$numOfPages = ceil($count / $limit);
 
 //prepare
 $stmt = $dbc->prepare('SELECT * FROM national_parks LIMIT :limit OFFSET :offset');
@@ -17,22 +21,72 @@ $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
 $stmt->execute();
 $parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$count = $dbc->query('SELECT count(*) FROM national_parks')->fetchColumn();
-
-$numOfPages = floor($count / $limit);
-
 if (!empty($_POST)){
     $stmt = $dbc->prepare('INSERT INTO national_parks (name, location, date_established, area_in_acres, description)
             VALUES (:name, :location, :date_established, :area_in_acres, :description)');
 
     $stmt->bindValue(':name', escape(trim($_POST['name'])), PDO::PARAM_STR);
-    $stmt->bindValue(':location',  escape(trim($_POST['location'])),  PDO::PARAM_STR);
-    $stmt->bindValue(':date_established', escape(trim($_POST['date_established'])),  PDO::PARAM_STR);
-    $stmt->bindValue(':area_in_acres',  str_replace(",", "", escape(trim($_POST['area_in_acres']))),  PDO::PARAM_STR);
-    $stmt->bindValue(':description',  escape(trim($_POST['description'])),  PDO::PARAM_STR);
+    $stmt->bindValue(':location',  escape(trim($_POST['location'])), PDO::PARAM_STR);
+    $stmt->bindValue(':date_established', escape(trim($_POST['date_established'])), PDO::PARAM_STR);
+    $stmt->bindValue(':area_in_acres',  str_replace(",", "", escape(trim($_POST['area_in_acres']))), PDO::PARAM_STR);
+    $stmt->bindValue(':description',  escape(trim($_POST['description'])), PDO::PARAM_STR);
 
     $stmt->execute();
 }
+
+$states = array(
+    'AL'=>'Alabama',
+    'AK'=>'Alaska',
+    'AZ'=>'Arizona',
+    'AR'=>'Arkansas',
+    'CA'=>'California',
+    'CO'=>'Colorado',
+    'CT'=>'Connecticut',
+    'DE'=>'Delaware',
+    'DC'=>'District of Columbia',
+    'FL'=>'Florida',
+    'GA'=>'Georgia',
+    'HI'=>'Hawaii',
+    'ID'=>'Idaho',
+    'IL'=>'Illinois',
+    'IN'=>'Indiana',
+    'IA'=>'Iowa',
+    'KS'=>'Kansas',
+    'KY'=>'Kentucky',
+    'LA'=>'Louisiana',
+    'ME'=>'Maine',
+    'MD'=>'Maryland',
+    'MA'=>'Massachusetts',
+    'MI'=>'Michigan',
+    'MN'=>'Minnesota',
+    'MS'=>'Mississippi',
+    'MO'=>'Missouri',
+    'MT'=>'Montana',
+    'NE'=>'Nebraska',
+    'NV'=>'Nevada',
+    'NH'=>'New Hampshire',
+    'NJ'=>'New Jersey',
+    'NM'=>'New Mexico',
+    'NY'=>'New York',
+    'NC'=>'North Carolina',
+    'ND'=>'North Dakota',
+    'OH'=>'Ohio',
+    'OK'=>'Oklahoma',
+    'OR'=>'Oregon',
+    'PA'=>'Pennsylvania',
+    'RI'=>'Rhode Island',
+    'SC'=>'South Carolina',
+    'SD'=>'South Dakota',
+    'TN'=>'Tennessee',
+    'TX'=>'Texas',
+    'UT'=>'Utah',
+    'VT'=>'Vermont',
+    'VA'=>'Virginia',
+    'WA'=>'Washington',
+    'WV'=>'West Virginia',
+    'WI'=>'Wisconsin',
+    'WY'=>'Wyoming',
+);
 ?>
 
 <!DOCTYPE html>
@@ -47,7 +101,6 @@ if (!empty($_POST)){
     <div id="container">
         <table>
             <tr>
-                <th>ID</th>
                 <th>Name</th>
                 <th>Location</th>
                 <th>Date Established</th>
@@ -57,10 +110,9 @@ if (!empty($_POST)){
 
             <? foreach ($parks as $park): ?>
                 <tr>
-                    <td><?= $park['id']; ?></td>
                     <td><?= $park['name']; ?></td>
                     <td><?= $park['location']; ?></td>
-                    <td><?= date_format(date_create($park['date_established']), 'F, j, Y'); ?></td>
+                    <td><?= date_format(date_create($park['date_established']), 'F j, Y'); ?></td>
                     <td><?= number_format($park['area_in_acres']); ?></td>
                     <td><?= $park['description']; ?></td>
                 </tr>
@@ -68,7 +120,7 @@ if (!empty($_POST)){
         </table>
 
         <div id='links'>
-            <? if ($page != 0): ?>
+            <? if ($page != 1): ?>
                 <a href="?page=<?= ($page - 1); ?>">Previous Page</a> 
             <? endif; ?>
 
@@ -76,27 +128,30 @@ if (!empty($_POST)){
                 <a href="?page=<?= ($page + 1); ?>">Next Page</a>
             <? endif; ?>
         </div>
+    </div>
 
-        <div id='form'>
-            <form method="POST">
-                <label>Park Name:</label>
-                <input id="name" type="text" name="name" placeholder="Park Name">
+    <div id='form'>
+        <form method="POST">
+            <label>Park Name:</label>
+            <input id="name" type="text" name="name" placeholder="Park Name">
 
-                <label>Location: </label>
-                <input id="location" type="text" name="location" placeholder="ie: Texas"><br>
+            <label>State: </label>
+            <select id="location" name="location">
+                <?foreach ($states as $state):?>
+                    <option><?= $state; ?></option>
+                <? endforeach?>
+            </select><br>
 
-                <label>Date Established: </label>
-                <input id="date_established" type="text" name="date_established" placeholder="YYYY-MM-DD">
+            <label>Date Established: </label>
+            <input id="date_established" type="date" name="date_established">
 
-                <label>Area (in acres): </label>
-                <input id="area_in_acres" type="text" name="area_in_acres" placeholder="Area in acres"><br>
+            <label>Area (in acres): </label>
+            <input id="area_in_acres" type="text" name="area_in_acres" placeholder="Area in acres"><br>
 
-                <label>Description: </label>
-                <input id="description" type="text" name="description" placeholder="Max: 500 charactrers"><br>
-                <button id="submit" type="submit">Submit</button>
-            </form>
-        </div>
-
+            <label>Description: </label><br>
+            <textarea id="description" type="text" name="description" placeholder="Max: 500 characters"></textarea><br>
+            <button id="submit" type="submit">Submit</button>
+        </form>
     </div>
 
 </body>
